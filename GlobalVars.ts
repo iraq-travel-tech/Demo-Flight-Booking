ef7b4d3391eaf8588a7b21f18fdef8a2638638eb
omar chatin <omerchatin19@gmail.com>
2023-02-13 21:48:09 +0300

    d

--- GlobalVars.ts
+++ GlobalVars.ts
@@ -1,4 +1,4 @@
 export const BASEURL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : `https://${process.env.VERCEL_URL}`;
+    : "https://travel-website-mu.vercel.app";

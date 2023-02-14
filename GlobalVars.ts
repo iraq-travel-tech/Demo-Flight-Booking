40988365bf1eed4ab8293b7d34572c9ca691d235
omar chatin <omerchatin19@gmail.com>
2023-02-14 18:17:39 +0300

    test

--- GlobalVars.ts
+++ GlobalVars.ts
@@ -1,14 +1,13 @@
 export let BASEURL: string;
 
+if (process.env.NODE_ENV === "development") {
+  BASEURL = `http://localhost:3000`;
+}
 if (
-  process.env.NODE_ENV === "development" &&
+  process.env.NODE_ENV === "production" &&
   process.env.NEXT_PUBLIC_VERCEL_URL
 ) {
   BASEURL = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
-} else if (process.env.NODE_ENV === "production") {
-  BASEURL = "https://travel-website-mu.vercel.app";
-} else if (process.env.NODE_ENV === "development") {
-  BASEURL = "http://localhost:3000";
 } else {
   BASEURL = "https://demo.iraqtraveltech.com";
 }

b479ad19fef48078062808a2f4f5fe20b7e4cf52
omar chatin <omerchatin19@gmail.com>
2023-02-15 12:47:03 +0300

    s

--- GlobalVars.ts
+++ GlobalVars.ts
@@ -1,12 +1,7 @@
 export let BASEURL: string;
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NEXT_PUBLIC_VERCEL_URL) {
   BASEURL = `http://localhost:3000`;
-} else if (
-  process.env.NODE_ENV === "production" &&
-  process.env.NEXT_PUBLIC_VERCEL_URL
-) {
-  BASEURL = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
 } else {
   BASEURL = "https://demo.iraqtraveltech.com";
 }

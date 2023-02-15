51455acbcadfdeb30d776e71fdd1101f41f8c17e
omar chatin <omerchatin19@gmail.com>
2023-02-15 13:38:35 +0300

    d

--- GlobalVars.ts
+++ GlobalVars.ts
@@ -1,7 +1,9 @@
 export let BASEURL: string;
 
 if (process.env.NEXT_PUBLIC_VERCEL_URL) {
-  if (process.env.NODE_ENV === "production") {
+  if (process.env.MAIN_VERCEL_URL) {
+    BASEURL = `${process.env.MAIN_VERCEL_URL}`;
+  } else if (process.env.NODE_ENV === "production") {
     BASEURL = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
   } else {
     BASEURL = `${process.env.NEXT_PUBLIC_VERCEL_URL}`;

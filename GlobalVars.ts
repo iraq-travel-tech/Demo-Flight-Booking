492c4afba8be2bf07ca00dbe7fed21d9a50d7def
omar chatin <omerchatin19@gmail.com>
2023-03-02 20:36:30 +0300

    s

--- GlobalVars.ts
+++ GlobalVars.ts
@@ -3,8 +3,8 @@ export let BASEURL: string;
 if (process.env.NODE_ENV === "development") {
   BASEURL = "http://localhost:3000";
 }
-if (process.env.BUILDTIME_URL) {
-  BASEURL = process.env.BUILDTIME_URL;
+if (process.env.NEXT_PUBLIC_BUILDTIME_URL) {
+  BASEURL = process.env.NEXT_PUBLIC_BUILDTIME_URL;
 } else {
   BASEURL = "https://demo.iraqtraveltech.com";
 }
